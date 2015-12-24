9a5e8a4c6a93116d12a6dd45ba576428738f3133
Josh Orndorff <admin@joshorndorff.com>
2015-12-24 14:38:08 -0500

    Updated interface to allow entering guesses in a more friendly manner.

--- helperFunctions.py
+++ helperFunctions.py
@@ -1,124 +1,173 @@
 from os import system, name
 
 def int_input(prompt, min = None, max = None):
-	"""
-		Allows the user to input an integer,
-		and asks for another try if a non-integer is entered.
-	"""
-	answer = raw_input(prompt)
-	try:
-		answer = int(answer)
-	except ValueError:
-		answer = int_input("That isn't a integer dude, please try again. ", min, max)
+  """
+    Allows the user to input an integer,
+    and asks for another try if a non-integer is entered.
+  """
+  answer = raw_input(prompt)
+  try:
+    answer = int(answer)
+  except ValueError:
+    answer = int_input("That isn't a integer dude, please try again. ", min, max)
 
-	while min and answer < min:
-		answer = int_input("Number must be at least {}. ".format(min), min, max)
+  while min and answer < min:
+    answer = int_input("Number must be at least {}. ".format(min), min, max)
 
-	while max and answer > max:
-		answer = int_input("Number must not be more than {}. ".format(max), min, max)
+  while max and answer > max:
+    answer = int_input("Number must not be more than {}. ".format(max), min, max)
 
-	return answer
+  return answer
 
 def yn_input(prompt):
-	"""
-		Allows the user to answer yes / no questions,
-		and asks for another try if a non-integer is entered.
-	"""
-	answer = raw_input(prompt).lower()
-	if answer in ["yes", "y"]:
-		return True
-	elif answer in ["no", "n"]:
-		return False
-	else:
-		return yn_input("Please type yes, no, y, or n. ")
+  """
+    Allows the user to answer yes / no questions,
+    and asks for another try if a non-integer is entered.
+  """
+  answer = raw_input(prompt).lower()
+  if answer in ["yes", "y"]:
+    return True
+  elif answer in ["no", "n"]:
+    return False
+  else:
+    return yn_input("Please type yes, no, y, or n. ")
 
 def clear():
-	''' Simple cross-platform screen clearing function'''
-	system('cls' if name == 'nt' else 'clear')
+  ''' Simple cross-platform screen clearing function'''
+  system('cls' if name == 'nt' else 'clear')
 
 def int_choice(prompt, choices, texts = None):
 
-	if texts == None:
-		texts = choices
-	elif len(texts) != len(choices):
-		raise ValueError("Choices and Texts must be the same length. ")
+  if texts == None:
+    texts = choices
+  elif len(texts) != len(choices):
+    raise ValueError("Choices and Texts must be the same length. ")
 
-	print(prompt)
+  print(prompt)
 
-	for i in range(len(choices)):
-		print("{}. {}".format(i, texts[i]))
+  for i in range(len(choices)):
+    print("{}. {}".format(i, texts[i]))
 
-	numAnswer = int_input("Choose an integer. ", 0, len(choices) - 1)
-	
-	return choices[numAnswer]
-	
+  numAnswer = int_input("Choose an integer. ", 0, len(choices) - 1)
+  
+  return choices[numAnswer]
+  
+def explain_guessing():
+  print("Explanation is not yet implemented. Sorry. Good Luck.")
 
 def print_hands(game):
-	'''
-	Prints all known information about players' hands.
-	'''
+  '''
+  Prints all known information about players' hands.
+  '''
 
-	# Print the title row
-	print(" " * 13 + "\t"),
-	for player in game.players:
-		print("{0:>13}".format(player.name)),
+  # Print the title row
+  print(" " * 13 + "\t"),
+  for player in game.players:
+    print("{0:>13}".format(player.name)),
 
-	for category in game.deck.categories:
-		print("\n--------{}-------".format(category)),
-		for card in game.deck:
-			if card.category == category:
-				print("\n{0:>13}\t".format(card.name)),
-				for player in game.players:
-					if card in player.has:
-						print("{0:>13}".format("  Have It ")),
-					elif card in player.hasnt:
-						print("{0:>13}".format("Don't Have")),
-					else:
-						print("{0:>13}".format("?-?-?-?-?-")),
+  for category in game.deck.categories:
+    print("\n--------{}-------".format(category)),
+    for card in game.deck:
+      if card.category == category:
+        print("\n{0:>13}\t".format(card.name)),
+        for player in game.players:
+          if card in player.has:
+            print("{0:>13}".format("  Have It ")),
+          elif card in player.hasnt:
+            print("{0:>13}".format("Don't Have")),
+          else:
+            print("{0:>13}".format("?-?-?-?-?-")),
 
 def print_history(game):
-	'''
-	Prints the record of all turns in the game so far.
-	'''
+  '''
+  Prints the record of all turns in the game so far.
+  '''
 
-	if len(game.history) == 0:
-		print("No turns yet")
-		return
+  if len(game.history) == 0:
+    print("No turns yet")
+    return
 
 
-	for turn in game.history:
-		if turn["guess"] == None: # Passed turn
-			print("{} passed.".format(turn["guesser"].name))
+  for turn in game.history:
+    if turn["guess"] == None: # Passed turn
+      print("{} passed.".format(turn["guesser"].name))
 
-		elif turn["disprover"] != None: # Normal turn
-			print("{} guessed [{}, {}, {}] and {} showed a card.".format(\
-			turn["guesser"].name,\
-			turn["guess"][0].name, turn["guess"][1].name, turn["guess"][2].name,\
-			turn["disprover"].name\
-			))
+    elif turn["disprover"] != None: # Normal turn
+      print("{} guessed [{}, {}, {}] and {} showed a card.".format(\
+      turn["guesser"].name,\
+      turn["guess"][0].name, turn["guess"][1].name, turn["guess"][2].name,\
+      turn["disprover"].name\
+      ))
 
-		else: # Winning
-			print("{} guessed [{}, {}, {}] and won.".format\
-			     (turn["guesser"].name, turn["guess"][0], turn["guess"][1], turn["guess"][2]))
-			
+    else: # Winning
+      print("{} guessed [{}, {}, {}] and won.".format\
+           (turn["guesser"].name, turn["guess"][0], turn["guess"][1], turn["guess"][2]))
+      
 
 def print_disproofs(game):
-	'''
-	Prints a list of all of a player's disproofs.
+  '''
+  Prints a list of all of a player's disproofs.
 
-	A disproof is a list of cards of which the player has at least one as
-	evidenced by the player having shown an unknown card earlier in the game.
-	'''
+  A disproof is a list of cards of which the player has at least one as
+  evidenced by the player having shown an unknown card earlier in the game.
+  '''
 
-	for player in game.players:
-		if len(player.disproofs) == 0:
-			raw_input("{} has no disproofs yet. Press enter to continue...".format(player.name))
-			continue
+  for player in game.players:
+    print("---{}'s disproofs---".format(player.name))
+    if len(player.disproofs) == 0:
+      print("{} has no disproofs.\n".format(player.name))
+      continue
+    for disproof in player.disproofs:
+      for card in disproof:
+        print("{0:>12}\t".format(card.name)),
+      print("")
 
-		print("---{}'s disproofs---".format(player.name))
-		for disproof in player.disproofs:
-			for card in disproof:
-				print("{0:>12}\t".format(card.name)),
-			print("")
-		raw_input("Press enter to continue...")
+def parse_guess(shorthand, deck):
+  '''
+  Returns a list of cards (one from each category) given a shorthand string.
+  Raises a ValueError if shorthand string is not valid.
+  '''
+  guess = []
+  sortedGuess = []
+  
+  # Split shorthand into individual entries, and make sure the number is correct.
+  shorthand = shorthand.lower().split()
+  if len(shorthand) != len(deck.categories):
+    raise ValueError("Shorthand must contain exactly {} entries.".format(len(deck)))
+    
+  # Find a card that "matches" each shorthand entry
+  for item in shorthand:
+    matchedOne = False
+    for card in deck:
+      # Compute the possible matching names. This is not trivial because of two-word cards.
+      toMatch = card.name.lower().split()
+      for x in toMatch:
+        x = x[:len(item)]
+        if item == x:
+          if not matchedOne:
+            guess.append(card)
+            matchedOne = True
+          else:
+            raise ValueError("Shorthand item {} is not unique.".format(item))
+  
+  # Make sure there is one card from each category
+  for category in deck.categories:
+    cardFound = False
+    for card in guess:
+      if card in deck.get_cards_by_category(category):
+        cardFound = True
+        # And put them in the correct order.
+        sortedGuess.append(card)
+    if not cardFound:
+      raise ValueError("No card found in category, {}.".format(category))
+  
+  return sortedGuess
+
+
+
+
+
+
+
+
 
--- textMode.py
+++ textMode.py
@@ -6,45 +6,51 @@ from os.path import isfile
 
 clear()
 
+# Get basic player info.
 playerNames = []
 name = None
 userPlayer = -1
 print("Enter all players' names in turn order. Blank to end.")
 while True:
-	name = raw_input("Player's name: ")
-	if userPlayer == -1 and yn_input("Is this you? "):
-		userPlayer = len(playerNames)
-	if name == "":
-		break
-	playerNames.append(name)
+  name = raw_input("Player's name: ")
+  if userPlayer == -1 and yn_input("Is this you? "):
+    userPlayer = len(playerNames)
+  if name == "":
+    break
+  playerNames.append(name)
 
+
+# Choose the deck
 if yn_input("Would you like to use the standard deck? "):
-	deck = "standard.deck"
+  deck = "standard.deck"
 else:
-	deck = ""
-	while not isfile(deck):
-		deck = raw_input("Please type the path to your custom deck: ")
-		# This works because the screen will be cleared immediately if the path is good.
-		print("\nSorry, that's not a valid path, please try again.")
+  deck = ""
+  while not isfile(deck):
+    deck = raw_input("Please type the path to your custom deck: ")
+    # This works because the screen is cleared immediately if the path is good.
+    print("\nSorry, that's not a valid path, please try again.")
 clear()
 
+# Create the game object
 game = clueGame(playerNames, userPlayer, deck)
 
+
+# Figure out which cards are in the user's hand.
 userCardCount = game.players[userPlayer].numCards
 
 while True:
-	userHand = []
+  userHand = []
 
-	for card in game.deck:
-		if len(userHand) < userCardCount:
-			if yn_input("Do you have the {}, {}? ".format(card.category, card.name)):
-				userHand.append(card)
+  for card in game.deck:
+    if len(userHand) < userCardCount:
+      if yn_input("Do you have the {}, {}? ".format(card.category, card.name)):
+        userHand.append(card)
 
-	if len(userHand) >= userCardCount:
-		break
-	else:
-		print("You didn't tell me enough cards. Let's start over.")
-		
+  if len(userHand) >= userCardCount:
+    break
+  else:
+    print("You didn't tell me enough cards. Let's start over.")
+    
 clear()
 
 game.set_user_hand(userHand)
@@ -52,64 +58,60 @@ game.set_user_hand(userHand)
 # -------------Keep the guessing going forever-------------------
 while True:
 
-	clear()
+  clear()
 
-	currentName = game.currentPlayer.name
-	print("It is {}'s turn.".format(currentName))
-	
-	print("1. {} made a guess.".format(currentName))
-	print("2. {} passed his turn.".format(currentName))
-	print("")
-	print("3. Data Table.")
-	print("4. Game History.")
-	print("5. Disproofs.")
-	print("")
-	option = int_input("Choose an option: ")
-	
-	if option == 1:
-		pass # This code comes later, after the else clause.
-	elif option == 2:
-		game.pass_turn()
-		continue
-	elif option == 3:
-		print_hands(game)
-		raw_input("\n\nPress enter to continue... ")
-		continue
-	elif option == 4:
-		print_history(game)
-		raw_input("\n\nPress enter to continue... ")
-		continue
-	elif option == 5:
-		print_disproofs(game)
-		continue
-	else:
-		raw_input("That isn't a valid option. Press any key to continue... ")
-	
-	
-	# They made a guess
-	guess = []
+  currentName = game.currentPlayer.name
+  print("It is {}'s turn.".format(currentName))
+  print  ("")
+  print("1. Explain how to guess.".format(currentName))
+  print("2. Pass turn.")
+  print("3. Data Table.")
+  print("4. Game History.")
+  print("5. Disproofs.")
+  print("")
+  option = raw_input(" Enter a guess or choose an option: ")
+  
+  if option == '1':
+    explain_guessing()
+    raw_input("\n\nPress enter to continue...")
+    continue
+  elif option == '2':
+    game.pass_turn()
+    continue
+  elif option == '3':
+    print_hands(game)
+    raw_input("\n\nPress enter to continue...")
+    continue
+  elif option == '4':
+    print_history(game)
+    raw_input("\n\nPress enter to continue...")
+    continue
+  elif option == '5':
+    print_disproofs(game)
+    raw_input("\n\nPress enter to continue...")
+    continue
+  
+  
+  # They made an actual guess
+  try:
+    guess = parse_guess(option, game.deck)
+  except ValueError:
+    raw_input("That is not a valid guess. Press enter to continue...")
+    continue
 
-	for category in game.deck.categories:
-		cards = game.deck.get_cards_by_category(category)
-		names = []
-		for card in cards:
-			names.append(card.name)
-		card = int_choice("Which {} did {} guess?".format(category, game.currentPlayer.name), cards, names)
-		guess.append(card)
-			
-	disprover = int_choice("Who had a disproving card? ", game.players, playerNames)
-			
-	if game.currentPlayer == game.userPlayer:
-		names = []
-		for card in guess:
-			names.append(card.name)
-		cardSeen = int_choice("Which card did you see?", guess, names)
-	else:
-		cardSeen = None
+  disprover = int_choice("Who had a disproving card? ", game.players[:]+[None], playerNames[:]+["Nobody"])
+      
+  if game.currentPlayer == game.userPlayer and disprover != None:
+    names = []
+    for card in guess:
+      names.append(card.name)
+    cardSeen = int_choice("Which card did you see?", guess, names)
+  else:
+    cardSeen = None
 
-	game.take_turn(guess, disprover, cardSeen)
-	
-	
-	
-	
-	
+  game.take_turn(guess, disprover, cardSeen)
+  
+  
+  
+  
+  
